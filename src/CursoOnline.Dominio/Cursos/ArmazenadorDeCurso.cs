87c35a8b417cca015cc8be53c27e6ed92b653a98
RafaelPerez de Lima <c3rafael.lima@carglass.com.br>
2021-12-26 17:28:57 -0300

    Teste para verificação de alteração de curso através do facade amarzenar curso

--- src/CursoOnline.Dominio/Cursos/ArmazenadorDeCurso.cs
+++ src/CursoOnline.Dominio/Cursos/ArmazenadorDeCurso.cs
@@ -15,14 +15,25 @@ namespace CursoOnline.Dominio.Cursos
         public void Armazenar(CursoDto cursoDto)
         {
             var cursoJaSalvo = _cursoRepositorio.ObterPeloNome(cursoDto.Nome);
+
             ValidadorDeRegra.Novo()
-                .Quando(cursoJaSalvo != null, "Nome do curso já consta no banco de dados")
-                .Quando(!Enum.TryParse<PublicoAlvo>(cursoDto.PublicoAlvo, out var publicoAlvo), "Publico Alvo inválido")
+                .Quando(cursoJaSalvo != null, Resource.NomeDoCursoJaExiste)
+                .Quando(!Enum.TryParse<PublicoAlvo>(cursoDto.PublicoAlvo, out var publicoAlvo), Resource.PublicoAlvoInvalido)
                 .DispararExcecaoSeExistir();
            
             var curso =
                 new Curso(cursoDto.Nome, cursoDto.Descricao, cursoDto.CargaHoraria, publicoAlvo, cursoDto.Valor);
-            _cursoRepositorio.Adicionar(curso);
+
+            if (cursoDto.Id > 0)
+            {
+                curso = _cursoRepositorio.ObterPorId(cursoDto.Id);
+                curso.AlterarNome(cursoDto.Nome);
+                curso.AlterarValor(cursoDto.Valor);
+                curso.AlterarCargaHoraria(cursoDto.CargaHoraria);
+            }
+
+            if(cursoDto.Id==0)
+                _cursoRepositorio.Adicionar(curso);
 
         }
     }
--- src/CursoOnline.Dominio/_Base/Resource.cs
+++ src/CursoOnline.Dominio/_Base/Resource.cs
@@ -5,6 +5,7 @@
         public const string NomeInvalido = "Nome Inválido.";
         public const string CargaHorariaInvalida = "Carga horária inválida.";
         public const string ValorInvalido = "Valor Inválido.";
-
+        public const string NomeDoCursoJaExiste = "Nome do curso já consta no banco de dados.";
+        public const string PublicoAlvoInvalido = "Publico Alvo inválido.";
     }
 }
